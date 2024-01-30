95003596b69ecf0490c56a782087764793c0ed45
skye-cyber <skye-cyber@gmail.com>
2024-01-30 15:17:10 +0300

    commit

---
+++ AFReader/main.py
@@ -0,0 +1,218 @@
+import curses
+import time
+import argparse
+
+
+def typewriter_effect(stdscr, text, speed):
+    curses.curs_set(0)
+    stdscr.clear()
+
+    row, col = 0, 0
+    max_row, max_col = stdscr.getmaxyx()
+
+    for line in text.split('\n'):
+        for char in line:
+            try:
+                stdscr.addch(row, col, char)
+            except curses.error:
+                col = 0
+                row = (row + 1) % max_row
+                stdscr.addch(row, col, char)
+
+            col += 1
+
+            if col == curses.COLS - 1:
+                col = 0
+                row += 1
+
+                if row == max_row - 1:
+                    stdscr.refresh()
+                    stdscr.getch()  # Wait for Enter key press
+                    stdscr.clear()
+                    row = 0
+
+            stdscr.refresh()
+            time.sleep(speed)
+
+        # Move to the next line after processing a line
+        col = 0
+        row += 1
+
+        # Check if the screen is full, and if so, scroll
+        if row == max_row - 1:
+            stdscr.refresh()
+            stdscr.getch()  # Wait for Enter key press
+            stdscr.clear()
+            row = 0
+
+    stdscr.getch()  # Wait for a key press before exiting
+
+
+def color_cycling_effect(stdscr, text, speed):
+    curses.curs_set(0)
+    stdscr.clear()
+
+    # Initialize color pairs
+    curses.start_color()
+    for i in range(1, curses.COLORS):
+        curses.init_pair(i, i, curses.COLOR_BLACK)
+
+    colors = list(range(1, curses.COLORS))
+
+    max_row, max_col = stdscr.getmaxyx()
+
+    col, row = 0, 0
+    for char in text:
+        if char == '\n':
+            col = 0
+            row += 1
+        else:
+            color_pair = curses.color_pair(colors[0])
+            stdscr.addch(row, col, char, color_pair)
+            col += 1
+
+        if col == max_col - 1:
+            col = 0
+            row += 1
+
+        if row == max_row - 1 and col == 0:  # Wait for key press only when the screen is full
+            stdscr.refresh()
+            stdscr.getch()  # wait for key press
+            stdscr.clear()  # clear screen
+            row = 0
+
+        stdscr.refresh()
+        time.sleep(speed)
+
+        # Rotate colors to ensure continuous cycling
+        colors.append(colors.pop(0))
+
+    stdscr.getch()
+
+
+def italic_in_effect(stdscr, text, speed):
+    curses.curs_set(0)
+    stdscr.clear()
+
+    row, col = 0, 0
+    max_row, max_col = stdscr.getmaxyx()
+
+    for char in text:
+        if char == '\n':
+            col = 0
+            row += 1
+        else:
+            # Use A_DIM for fading effect
+            stdscr.addch(row, col, char, curses.A_ITALIC)
+            col += 1
+
+        if col == max_col - 1:
+            col = 0
+            row += 1
+
+        if row == max_row - 1 and col == 0:
+            stdscr.refresh()
+            stdscr.getch()  # Wait for key press to continue
+            stdscr.clear()
+            row = 0
+
+        time.sleep(speed)
+        stdscr.refresh()
+
+    stdscr.getch()  # Wait for a key press before exiting
+
+
+def DIM_text_effect(stdscr, text, speed):
+    curses.curs_set(0)
+    stdscr.clear()
+
+    row, col = 0, 0
+    max_row, max_col = stdscr.getmaxyx()
+
+    for char in text:
+        if char == '\n':
+            col = 0
+            row += 1
+        else:
+            # Use A_DIM for fading effect
+            stdscr.addch(row, col, char, curses.A_DIM)
+            col += 1
+
+        if col == max_col - 1:
+            col = 0
+            row += 1
+
+        if row == max_row - 1 and col == 0:
+            stdscr.refresh()
+            stdscr.getch()  # Wait for key press to continue
+            stdscr.clear()
+            row = 0
+
+        time.sleep(speed)
+        stdscr.refresh()
+
+    stdscr.getch()  # Wait for a key press before exiting
+
+
+def read_text_from_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+
+def interactive_menu(stdscr):
+    curses.curs_set(0)
+    stdscr.clear()
+
+    options = ["Typewriter Effect", "Italic Text", "Color Cycling Effect",
+               "Fade In Effect(DIM Text)"]
+
+    selected_option = 0
+
+    while True:
+        stdscr.clear()
+
+        for i, option in enumerate(options):
+            stdscr.addstr(i + 1, 1, f"{i + 1}. {option}",
+                          curses.A_BOLD if i == selected_option else curses.A_NORMAL)
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and selected_option > 0:
+            selected_option -= 1
+        elif key == curses.KEY_DOWN and selected_option < len(options) - 1:
+            selected_option += 1
+        elif key == 10:  # Enter key
+            return selected_option
+
+
+def main(stdscr):
+    # create argument parser
+    parser = argparse.ArgumentParser(description='''Open documents in anymated mode''')
+    parser.add_argument('-i', '--input_file', help='file to to open', required=True)
+    parser.add_argument('-s', '--speed', default=0.001, type=float, help='Text display speed')
+
+    args = parser.parse_args()
+    input_file = args.input_file
+    speed = args.speed
+
+    with open(input_file, 'r') as file:
+        text_content = file.read()
+
+    selected_option = interactive_menu(stdscr)
+
+    if selected_option == 0:
+        typewriter_effect(stdscr, text_content, speed)
+    elif selected_option == 1:
+        italic_in_effect(stdscr, text_content, speed)
+    elif selected_option == 2:
+        color_cycling_effect(stdscr, text_content, speed)
+    elif selected_option == 3:
+        DIM_text_effect(stdscr, text_content, speed)
+    # Add more conditions for additional animation styles
+
+
+if __name__ == '__main__':
+    try:
+        curses.wrapper(main)
+    except KeyboardInterrupt:
+        pass
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='AFReader',
+    version='1.0',
+    author="Wambua aka skye-cyber",
+    packages=["AFReader"],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            "AFReader=main:main"],
+    },
+    python_requires='>=3.6',
+    install_requires=[
+        'argparse',
+        'curses'
+    ],
+    include_package_data=True,
+    license="MIT",
+    keywords='AFReader',
+    classifiers=[
+        "Environment :: Console",
+        "Operating System :: POSIX :: Linux",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: PyPi",
+    ],
+)
